8bc91330369cead6b354ae8d492447d54f418c0c
Eduardo Torres Ferreira <eduardotorres@jera.com.br>
2021-01-24 22:27:52 -0400

    Refactoring fragment

--- app/src/main/java/com/example/rocketreserver/Apollo.kt
+++ app/src/main/java/com/example/rocketreserver/Apollo.kt
@@ -1,10 +1,22 @@
 package com.example.rocketreserver
 
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.await
 
 class Apollo {
     val graphqlURL = "https://apollo-fullstack-tutorial.herokuapp.com"
     val client = ApolloClient.builder()
         .serverUrl(graphqlURL)
         .build()
+
+    suspend fun queryLaunches() : List<LaunchListQuery.Launch>?{
+        return try {
+            client.query(LaunchListQuery()).await()?.data?.launches?.launches?.filterNotNull()
+        }catch (e : Throwable){
+            Log.d("LaunchList", "Failure", e)
+            null
+        }
+    }
 }
--- app/src/main/java/com/example/rocketreserver/LaunchListFragment.kt
+++ app/src/main/java/com/example/rocketreserver/LaunchListFragment.kt
@@ -14,7 +14,11 @@ import com.example.rocketreserver.databinding.LaunchListFragmentBinding
 class LaunchListFragment : Fragment() {
     private lateinit var binding: LaunchListFragmentBinding
     val apollo = Apollo()
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = LaunchListFragmentBinding.inflate(inflater)
         return binding.root
     }
@@ -22,19 +26,12 @@ class LaunchListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         lifecycleScope.launchWhenResumed {
-            try {
-                apollo.client.query(LaunchListQuery()).await()?.let { response ->
-                    response.data?.launches?.let {
-                        val adapter = LaunchListAdapter(it.launches.filterNotNull())
-                        binding.launches.layoutManager = LinearLayoutManager(requireContext())
-                        binding.launches.adapter = adapter
-                    }
-
-                }
-            }catch (e : Throwable){
-                Log.d("LaunchList", "Failure", e)
-            }
-
+            apollo.queryLaunches()?.let { setupAdapter(it) }
         }
+    }
+
+    private fun setupAdapter(list: List<LaunchListQuery.Launch>) {
+        binding.launches.layoutManager = LinearLayoutManager(requireContext())
+        binding.launches.adapter = LaunchListAdapter(list)
     }
 }
